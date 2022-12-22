e95168d89821ae88304fc26090027187a6ba6444
Hassan Ali <121165697+Hassan-html@users.noreply.github.com>
2022-12-22 13:00:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const openit=document.getElementById('open-btn');
+const Menu=document.getElementById('menu');
+const closeit=document.querySelector('.close');
+const Mainimg=document.querySelector('.head-img');
+
+openit.addEventListener('click',function () {
+    Menu.classList.toggle('open');
+}
+)
+closeit.addEventListener('click',function () {
+    Menu.classList.toggle('open');
+}
+)
+
+
+
